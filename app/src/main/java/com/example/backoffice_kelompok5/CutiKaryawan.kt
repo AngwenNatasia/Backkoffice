82af5e53f22523ea751caa8a1462ee8a8202f32a
jevinaliviani <jevina22ti@mahasiswa.pcr.ac.id>
2024-12-05 14:19:59 +0700

    adaPresensi

--- app/src/main/java/com/example/backoffice_kelompok5/CutiKaryawan.kt
+++ app/src/main/java/com/example/backoffice_kelompok5/CutiKaryawan.kt
@@ -1,59 +1,50 @@
 package com.example.backoffice_kelompok5
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CutiKaryawan.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CutiKaryawan : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cuti_karyawan, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CutiKaryawan.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CutiKaryawan().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        val view = inflater.inflate(R.layout.fragment_cuti_karyawan, container, false)
+
+        val nama = view.findViewById<EditText>(R.id.nama)
+        val divisi = view.findViewById<EditText>(R.id.divisi)
+        val status = view.findViewById<EditText>(R.id.status)
+        val lamaCuti = view.findViewById<EditText>(R.id.lama_Cuti)
+        val keterangan = view.findViewById<EditText>(R.id.keterangan)
+        val tanggal = view.findViewById<EditText>(R.id.tanggal_cuti)
+        val submitButton = view.findViewById<Button>(R.id.submit)
+
+        submitButton.setOnClickListener {
+            val namaKaryawan = nama.text.toString()
+            val divisiKaryawan = divisi.text.toString()
+            val statusKaryawan = status.text.toString()
+            val lama = lamaCuti.text.toString()
+            val tanggal = tanggal.text.toString()
+            val keterangan = keterangan.text.toString()
+
+            if (namaKaryawan.isNotEmpty() && divisiKaryawan.isNotEmpty() && keterangan.isNotEmpty() && lama.isNotEmpty() && statusKaryawan.isNotEmpty() && tanggal.isNotEmpty()) {
+                Toast.makeText(
+                    requireContext(),
+                    "$namaKaryawan dari divisi $divisiKaryawan cuti karena $keterangan",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(requireContext(), "Harap isi semua field.", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        return view
     }
 }
